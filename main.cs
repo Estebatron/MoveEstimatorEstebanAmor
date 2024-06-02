0c9cc32015b8fbcc40e604e2ec4ac7c85ecba746
Esteban_Amor <145415808+Estebatron@users.noreply.github.com>
2024-06-02 00:59:28 +0000

    Coded Everything

--- main.cs
+++ main.cs
@@ -2,6 +2,26 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+
+    Console.WriteLine ("Please Input the number of hours worked");
+    var H = Console.ReadLine();
+    Console.WriteLine ("Please Input the number of miles traveled");
+    var M = Console.ReadLine();
+
+    var splitH = H.Split(' ');
+    var splitM = M.Split(' ');
+
+    var Hours = splitH[0];
+    var Miles = splitM[0];
+
+    double HoursWorked;
+    double MilesTraveled;
+
+    if (double.TryParse(Hours, out HoursWorked) && double.TryParse(Miles, out MilesTraveled)){
+      Console.WriteLine("$"+(200 + (MilesTraveled * 2) + (HoursWorked * 150)));
+    }
+    else{
+      Console.WriteLine("Invalid Input");
+    }
   }
 }
